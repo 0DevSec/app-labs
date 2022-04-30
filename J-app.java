84c9b91664661ca2a5c5a04d8b78c835356c2521
dev <dev.beats57@gmail.com>
2022-04-30 06:58:26 +0300

    uplode-file

---
+++ J-app.java
@@ -0,0 +1,35 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.just.tryy;
+import java.util.Scanner;  // Import the Scanner class
+
+/**
+ *
+ * @author dev
+ */
+public class JustTryy {
+
+    public static void main(String[] args) {
+          //dev
+
+//int a = -5 + 8 * 6 ;
+//int b = (55+9) % 9;
+//int c = 20 + -3 * 5 / 8;
+//int d = 5 + 15 / 3 *2 - 8 % 3;
+
+Scanner myObj = new Scanner(System.in);
+System.out.println("* Enter The Number >>");
+int userInput = myObj.nextInt();
+int userInput1 = myObj.nextInt();
+
+
+
+        System.out.println("Hello\n"+"DeV\n"+ userInput * userInput1);
+
+
+    }
+}
+
